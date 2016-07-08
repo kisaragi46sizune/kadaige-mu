2d8e70a68e42d3f1553a28a964cb0c80b457cae7
kisaragi46sizune <sizune46kisaragi@gmail.com>
2016-07-08 12:23:13 +0900

    0708

--- kadaige-mu/Form1.cs
+++ kadaige-mu/Form1.cs
@@ -12,6 +12,13 @@ namespace kadaige_mu
 {
     public partial class Form1 : Form
     {
+        static Random rand = new Random();
+        //敵の速度
+        const float ENEMY_SPEED = 10f;
+        //アイテムの最大速度
+        const float ITEM_SPEED = 10f;
+        //アイテムの残り数
+        int iItemCount = 0;
         enum SCENES
         {
             SC_NONE,//無効
@@ -101,6 +108,33 @@ namespace kadaige_mu
                     py[0] = (ClientSize.Height - labels[0].Height) / 2;
                     labels[0].Left = (int)px[0];
                     labels[0].Top = (int)py[0];
+
+                    //敵の初期化
+                    for (int i = 1; i < 1 + ENEMY_MAX; i++)
+                    {
+                        type[i] = CHRTIPYPE.CHRTIPYPE_ENEMY;
+                        vx[i] = (float)(rand.NextDouble() * (2 * ENEMY_SPEED) - ENEMY_SPEED);
+                        vy[i] = (float)(rand.NextDouble() * (2 * ENEMY_SPEED) - ENEMY_SPEED);
+                        labels[i].Text = "♂";
+                        px[i] = rand.Next(ClientSize.Width - labels[i].Width);
+                        py[i] = rand.Next(ClientSize.Height - labels[i].Height);
+                    }
+
+                    //アイテムの初期化
+                    for (int i = 1+ENEMY_MAX; i < CHR_MAX; i++)
+                    {
+                        type[i] = CHRTIPYPE.CHRTIPYPE_ITEM;
+                        vx[i] = (float)(rand.NextDouble() * (2 * ITEM_SPEED) - ITEM_SPEED);
+                        vy[i] = (float)(rand.NextDouble() * (2 * ITEM_SPEED) - ITEM_SPEED);
+                        labels[i].Text = "♀";
+                        px[i] = rand.Next(ClientSize.Width - labels[i].Width);
+                        py[i] = rand.Next(ClientSize.Height - labels[i].Height);
+                    }
+                    //アイテムの残り数を設定
+                    iItemCount = ITEM_MAX;
+
+
+
                     break;
             }
         }
@@ -140,8 +174,81 @@ namespace kadaige_mu
                     case CHRTIPYPE.CHRTIPYPE_PLAYER:
                         updataPlayer(i);
                         break;
+                    case CHRTIPYPE.CHRTIPYPE_ENEMY:
+                        updateEnemy(i);
+                        break;
+                    case CHRTIPYPE.CHRTIPYPE_ITEM:
+                        updateItem(i);
+                        break;
                 }
             }
+        }
+        //敵の更新処理
+        private void updateEnemy(int i)
+        {
+            constantMove(i);
+            if (hitPlayer(i))
+            {
+                nextScene = SCENES.SC_GAMEOVER;
+            }
+        }
+
+        //等速直線運動
+        private void constantMove(int i)
+        {
+            px[i] += vx[i];
+            py[i] += vy[i];
+
+
+            if (px[i] <= 0)
+            {
+                vx[i] = Math.Abs(vx[i]);
+            }
+
+            else if (px[i] > ClientSize.Width - labels[i].Width)
+            {
+                vx[i] = -Math.Abs(vx[i]);
+            }
+
+            if (py[i] <= 0)
+            {
+                vy[i] = Math.Abs(vy[i]);
+            }
+
+            else if (py[i] > ClientSize.Height - labels[i].Height)
+            {
+                vy[i] = -Math.Abs(vy[i]);
+            }
+
+
+        }
+        //アイテムの更新処理
+        private void updateItem(int i)
+        {
+            constantMove(i);
+            if (hitPlayer(i))
+            {
+                //アイテムを消す
+                type[i] = CHRTIPYPE.CHRTIPYPE_NONE;
+                iItemCount--;
+                if (iItemCount <= 0)
+                {
+                    //アイテムをすべてとった
+                    nextScene = SCENES.SC_CLEAR;
+                }
+            }
+        }
+
+        //pyレイヤーがぶつかっているか
+        //true=ぶつかってる false=ぶつかって内
+        private bool hitPlayer(int i)
+        {
+            
+            if ((px[0]<labels[i].Right)&&(labels[0].Right>px[i])&&(py[0]<labels[i].Bottom)&&(labels[0].Bottom>py[i]))
+            {
+                return true;
+            }
+            return false;
         }
         /**プレイヤーの更新処理*/
         private void updataPlayer(int i)
